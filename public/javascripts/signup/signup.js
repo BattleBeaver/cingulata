7fd0995a0c13e6f420009c90f74019b42f29c9e4
BattleBeaver <kuzmentsov@gmail.com>
2016-06-12 15:35:55 +0300

    Removed auth module due to unused

--- public/javascripts/signup/signup.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function() {
-  var form = document.getElementById("signupform");
-  document.getElementById("submit").addEventListener("click", function() {
-    cng.ajax.submit(form);
-  })
-});
